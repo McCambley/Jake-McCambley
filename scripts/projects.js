afe8e7f6e81c510a1d2bdb2dbb7c6f69d1fac9cf
McCambley <jakemccamb16@gmail.com>
2025-12-14 10:17:45 -0500

    Update content

--- scripts/projects.js
+++ scripts/projects.js
@@ -15,49 +15,49 @@ const projects = [
     repoLink: "",
     demoLink: "",
   },
+  // {
+  //   name: "NewsExplorer",
+  //   description:
+  //     "This application allows users to login and explore the day's news using the News API. Users can search for, read, and save articles according to their interests. This project is the capstone project of my education at TripleTen.",
+  //   tech: "React | TypeScript | MongoDB | Express | Node.js | Google Cloud | Styled-components",
+  //   imgSrc: "./images/newsexplorer.png",
+  //   imgAlt: "The news explorer project",
+  //   liveLink: "https://mccambley.github.io/news-explorer-frontend/",
+  //   repoLink: "https://github.com/McCambley/news-explorer-frontend",
+  //   demoLink: "https://youtu.be/q7IdngTegY8",
+  // },
+  // {
+  //   name: "Around the U.S.",
+  //   description:
+  //     "This application allows users to upload photos with custom captions to a personal photo collection. Users may also create a custom profile. I built modals windows and unique client-side form validation behavior to improve the user's experience.",
+  //   tech: "React | Express | MongoDB | Node.js | Google Cloud",
+  //   imgSrc: "./images/jean.jpeg",
+  //   imgAlt: "Shaggy hiker",
+  //   liveLink: "https://mccambley.github.io/react-around-api-full/",
+  //   repoLink: "https://github.com/McCambley/react-around-api-full",
+  //   demoLink: "https://youtu.be/z0kQNpBVMcw",
+  // },
+  // {
+  //   name: "DotCot",
+  //   description:
+  //     "An app designed to make it easier for investors and startups to connect, DotCot allows investors to easily create custom forms, share forms with founders in their network, and collect sortable descriptive data on new startups.",
+  //   tech: "React | Express | MongoDB | Node.js | CI-CD | Heroku | Passport | Mocha | Styled-components",
+  //   imgSrc: "./images/dotcot-phone.png",
+  //   imgAlt: "DotCot Phone",
+  //   liveLink: "",
+  //   repoLink: "https://github.com/McCambley/private-projects/tree/main/dotcot#dotcot-by-venturecat",
+  //   demoLink: "https://youtu.be/yGJ2WOfZvew",
+  // },
   {
-    name: "NewsExplorer",
+    name: "Orange Runner",
     description:
-      "This application allows users to login and explore the day's news using the News API. Users can search for, read, and save articles according to their interests. This project is the capstone project of my education at Practicum by Yandex.",
-    tech: "React | TypeScript | MongoDB | Express | Node.js | Google Cloud | Styled-components",
-    imgSrc: "./images/newsexplorer.png",
-    imgAlt: "The news explorer project",
-    liveLink: "https://mccambley.github.io/news-explorer-frontend/",
-    repoLink: "https://github.com/McCambley/news-explorer-frontend",
-    demoLink: "https://youtu.be/q7IdngTegY8",
-  },
-  {
-    name: "Around the U.S.",
-    description:
-      "This application allows users to upload photos with custom captions to a personal photo collection. Users may also create a custom profile. I built modals windows and unique client-side form validation behavior to improve the user's experience.",
-    tech: "React | Express | MongoDB | Node.js | Google Cloud",
-    imgSrc: "./images/jean.jpeg",
-    imgAlt: "Shaggy hiker",
-    liveLink: "https://mccambley.github.io/react-around-api-full/",
-    repoLink: "https://github.com/McCambley/react-around-api-full",
-    demoLink: "https://youtu.be/z0kQNpBVMcw",
-  },
-  {
-    name: "Ashley Fife's Portfolio",
-    description:
-      "A portfolio designed to show of the work and unique style of UI/UX designer Ashley Fife. This project was used as a way to explore the styling capabilities of Tailwind when paired with the performance benefits of Next.js. This website was designed by Ashley Fife.",
-    tech: "TypeScript | Next.js | Tailwind | Vercel",
-    imgSrc: "./images/ashley.png",
-    imgAlt: "Ashley Fife ",
-    liveLink: "https://ashleyfife.com",
-    repoLink: "https://github.com/McCambley/ashley-portfolio",
-    demoLink: "",
-  },
-  {
-    name: "DotCot",
-    description:
-      "An app designed to make it easier for investors and startups to connect, DotCot allows investors to easily create custom forms, share forms with founders in their network, and collect sortable descriptive data on new startups.",
-    tech: "React | Express | MongoDB | Node.js | CI-CD | Heroku | Passport | Mocha | Styled-components",
-    imgSrc: "./images/dotcot-phone.png",
+      "My brother, The Orange Runner, writes and illustrates a comic about running. As his biggest fan, I rebuilt his outdated WordPress site using Next.js with content published via Contentful. Static generation with server rendering on request mean faster performance with maximized SEO.",
+    tech: "Next.js | React | Node | styled-components | Contentful",
+    imgSrc: "./images/tor.png",
     imgAlt: "DotCot Phone",
-    liveLink: "",
-    repoLink: "https://github.com/McCambley/private-projects/tree/main/dotcot#dotcot-by-venturecat",
-    demoLink: "https://youtu.be/yGJ2WOfZvew",
+    liveLink: "https://the-orange-runner.vercel.app/",
+    repoLink: "https://github.com/McCambley/the-orange-runner",
+    demoLink: "https://youtu.be/G9VpHw2V0II",
   },
   {
     name: "Applets",
@@ -71,39 +71,38 @@ const projects = [
     demoLink: "",
   },
   {
-    name: "Orange Runner",
+    name: "Ashley Fife's Portfolio",
     description:
-      "My brother, The Orange Runner, writes and illustrates a comic about running. As his biggest fan, I rebuilt his outdated WordPress site using Next.js with content published via Contentful. Static generation with server rendering on request mean faster performance with maximized SEO.",
-    tech: "Next.js | React | Node | styled-components | Contentful",
-    imgSrc: "./images/tor.png",
-    imgAlt: "DotCot Phone",
-    liveLink: "https://the-orange-runner.vercel.app/",
-    repoLink: "https://github.com/McCambley/the-orange-runner",
-    demoLink: "https://youtu.be/G9VpHw2V0II",
+      "A portfolio designed to show of the work and unique style of UI/UX designer Ashley Fife. This project was used as a way to explore the styling capabilities of Tailwind when paired with the performance benefits of Next.js. This website was designed by Ashley Fife.",
+    tech: "TypeScript | Next.js | Tailwind | Vercel",
+    imgSrc: "./images/ashley.png",
+    imgAlt: "Ashley Fife ",
+    liveLink: "https://ashleyfife.com",
+    repoLink: "https://github.com/McCambley/ashley-portfolio",
+    demoLink: "",
   },
-  {
-    name: "Apiary",
-    description:
-      "A single page application built for Practicum by Yandex's Apiary program, which lets students build pages for startups in order to gain experience. Clients may update the page content via Contentful. Queries are sent using GraphQL and managed with Apollo Client. ",
-    tech: "React | Contentful | GraphQL | Apollo | Node",
-    imgSrc: "./images/apiary.png",
-    imgAlt: "Apiary",
-    liveLink: "https://mccambley.github.io/apiary/",
-    repoLink: "https://github.com/McCambley/apiary",
-    demoLink: "https://youtu.be/ztpOAcOPbu0",
-  },
-
-  {
-    name: "Aviary",
-    description:
-      "Aviary uses eBird, xeno-canto, and Unsplash to build a soothing audiovisual experience of birdcalls and landscape photography for users depending on their location and desired environment. The result of a 96-hour hackathon, this app was built to help users stay grounded during a global pandemic",
-    tech: "React | Figma | Node",
-    imgSrc: "./images/aviary.png",
-    imgAlt: "Bird silhouette",
-    liveLink: "https://mccambley.github.io/hacktober-code-jam/",
-    repoLink: "https://github.com/McCambley/hacktober-code-jam",
-    demoLink: "https://youtu.be/6hlaG0G9euE",
-  },
+  // {
+  //   name: "Apiary",
+  //   description:
+  //     "A single page application built for TripleTen's Apiary program, which lets students build pages for startups in order to gain experience. Clients may update the page content via Contentful. Queries are sent using GraphQL and managed with Apollo Client. ",
+  //   tech: "React | Contentful | GraphQL | Apollo | Node",
+  //   imgSrc: "./images/apiary.png",
+  //   imgAlt: "Apiary",
+  //   liveLink: "https://mccambley.github.io/apiary/",
+  //   repoLink: "https://github.com/McCambley/apiary",
+  //   demoLink: "https://youtu.be/ztpOAcOPbu0",
+  // },
+  // {
+  //   name: "Aviary",
+  //   description:
+  //     "Aviary uses eBird, xeno-canto, and Unsplash to build a soothing audiovisual experience of birdcalls and landscape photography for users depending on their location and desired environment. The result of a 96-hour hackathon, this app was built to help users stay grounded during a global pandemic",
+  //   tech: "React | Figma | Node",
+  //   imgSrc: "./images/aviary.png",
+  //   imgAlt: "Bird silhouette",
+  //   liveLink: "https://mccambley.github.io/hacktober-code-jam/",
+  //   repoLink: "https://github.com/McCambley/hacktober-code-jam",
+  //   demoLink: "https://youtu.be/6hlaG0G9euE",
+  // },
   {
     name: "U.S. & Racism",
     description:
@@ -115,27 +114,27 @@ const projects = [
     repoLink: "https://github.com/McCambley/US-Racism",
     demoLink: "https://youtu.be/cYGvG8u6Lxc",
   },
-  {
-    name: "Prim-U",
-    description: "Prim-U is a South African beauty service startup. I built them a homepage that achieves their goal of helping freelance beauticians connect with new customers.",
-    tech: "HTML | CSS | JS | Figma",
-    imgSrc: "./images/primu.png",
-    imgAlt: "Woman posing in fresh Makeup",
-    liveLink: "https://mccambley.github.io/Prim-U",
-    repoLink: "https://github.com/McCambley/Prim-U",
-    demoLink: "https://youtu.be/g9YNXG74x9o",
-  },
-  {
-    name: "Portland to Portland",
-    description:
-      "How can we make sure our page is accessible on any device? A foray into the world of responsiveness, this project fulfilled a Figma design and implemented media queries to ensure that the page appeared and functioned seamlessly across all device sizes.",
-    tech: "HTML | CSS | Figma",
-    imgSrc: "./images/lead-mountains.jpg",
-    imgAlt: "Portland to Portland",
-    liveLink: "https://mccambley.github.io/web_project_3/",
-    repoLink: "https://github.com/McCambley/web_project_3",
-    demoLink: "https://youtu.be/zEunkGz6Q74",
-  },
+  // {
+  //   name: "Prim-U",
+  //   description: "Prim-U is a South African beauty service startup. I built them a homepage that achieves their goal of helping freelance beauticians connect with new customers.",
+  //   tech: "HTML | CSS | JS | Figma",
+  //   imgSrc: "./images/primu.png",
+  //   imgAlt: "Woman posing in fresh Makeup",
+  //   liveLink: "https://mccambley.github.io/Prim-U",
+  //   repoLink: "https://github.com/McCambley/Prim-U",
+  //   demoLink: "https://youtu.be/g9YNXG74x9o",
+  // },
+  // {
+  //   name: "Portland to Portland",
+  //   description:
+  //     "How can we make sure our page is accessible on any device? A foray into the world of responsiveness, this project fulfilled a Figma design and implemented media queries to ensure that the page appeared and functioned seamlessly across all device sizes.",
+  //   tech: "HTML | CSS | Figma",
+  //   imgSrc: "./images/lead-mountains.jpg",
+  //   imgAlt: "Portland to Portland",
+  //   liveLink: "https://mccambley.github.io/web_project_3/",
+  //   repoLink: "https://github.com/McCambley/web_project_3",
+  //   demoLink: "https://youtu.be/zEunkGz6Q74",
+  // },
   // {
   //   name: "Learning how to learn",
   //   description: "This project is an exercise in how to communicate information more effectively using design layouts. This pixel perfect page was my first exercise in transforming an idea to an interactive website.",
@@ -159,17 +158,17 @@ const projects = [
     demoLink: "https://youtu.be/Gi7lSsnpxlY",
   },
 
-  {
-    name: "JS Playground",
-    description:
-      "This project is a collection of smaller projects built with the intention of learning the intricacies and capabilities of vanilla javascript prior to embarking on the journey of React. These projects also provided and oppurtunity to dive into UX/UI, as all twenty-seven projects include original designs ",
-    tech: "HTML5 | CSS | JS",
-    imgSrc: "./images/js30.jpeg",
-    imgAlt: "Pink clouds",
-    liveLink: "https://mccambley.github.io/JavaScript30/",
-    repoLink: "https://github.com/McCambley/JavaScript30",
-    demoLink: "https://youtu.be/8z9hugvI4-U",
-  },
+  // {
+  //   name: "JS Playground",
+  //   description:
+  //     "This project is a collection of smaller projects built with the intention of learning the intricacies and capabilities of vanilla javascript prior to embarking on the journey of React. These projects also provided and oppurtunity to dive into UX/UI, as all twenty-seven projects include original designs ",
+  //   tech: "HTML5 | CSS | JS",
+  //   imgSrc: "./images/js30.jpeg",
+  //   imgAlt: "Pink clouds",
+  //   liveLink: "https://mccambley.github.io/JavaScript30/",
+  //   repoLink: "https://github.com/McCambley/JavaScript30",
+  //   demoLink: "https://youtu.be/8z9hugvI4-U",
+  // },
 ];
 
 class Card {
